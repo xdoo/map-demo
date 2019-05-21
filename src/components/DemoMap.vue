e62a15a36eef695e63a21863ef98eb1529243b13
xdoo <claus.straube@gmail.com>
2019-05-21 08:49:07 +0200

    center marienplatz

--- src/components/DemoMap.vue
+++ src/components/DemoMap.vue
@@ -1,7 +1,8 @@
 <template>
     <div style="height: 100%; width: 100%; margin: 0">
     <l-map 
-      :zoom="zoom"  
+      :zoom="zoom" 
+      :center="center" 
       style="z-index: 1"
       v-on:update:center="showCenter($event)"
       v-on:update:zoom="showZoom($event)">
@@ -19,8 +20,8 @@ export default {
       zoom: 12,
       // hier muss dann die esri map referenziert werden
       url:'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
-      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      //center: L.latLng(48.1371181, 11.5755711), // Marienplatz
+      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      center: [48.1371181,11.5755711] // Marienplatz
     }
   },
   methods: {
